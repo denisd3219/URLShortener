346cc2764ec63731758fd0e4f2616416d2106e13
denisd3219 <79655498+denisd3219@users.noreply.github.com>
2022-01-01 18:00:57 +0300

    refactor: Shorten view uses bootstrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CopyToClipboard(id) {
+	var copyText = document.getElementById(id);
+	copyText.select();
+	copyText.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(copyText.value);
+}
+
+$(document).ready(function () {
+	$('[data-toggle="popover"]').popover();
+});
